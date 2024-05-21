13b646ba8b62445fef982ebb6ee2c0e0e80d01f1
Bruno Reis <bruno.reiis15@gmail.com>
2024-05-21 18:03:44 -0300

    feature: navbar

---
+++ config/site.ts
@@ -0,0 +1,81 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "NexusFT Web",
+  description: "A Website for Nexus",
+  navItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Sobre",
+      href: "/sobre",
+    },
+    {
+      label: "Equipe",
+      href: "/equipe",
+    },
+    // {
+    //   label: "Blog",
+    //   href: "/blog",
+    // },
+    // {
+    //   label: "About",
+    //   href: "/about",
+    // },
+  ],
+  navMenuItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Sobre",
+      href: "/sobre",
+    },
+    {
+      label: "Equipe",
+      href: "/equipe",
+    },
+    // {
+    //   label: "Profile",
+    //   href: "/profile",
+    // },
+    // {
+    //   label: "Dashboard",
+    //   href: "/dashboard",
+    // },
+    // {
+    //   label: "Projects",
+    //   href: "/projects",
+    // },
+    // {
+    //   label: "Team",
+    //   href: "/team",
+    // },
+    // {
+    //   label: "Calendar",
+    //   href: "/calendar",
+    // },
+    // {
+    //   label: "Settings",
+    //   href: "/settings",
+    // },
+    // {
+    //   label: "Help & Feedback",
+    //   href: "/help-feedback",
+    // },
+    // {
+    //   label: "Logout",
+    //   href: "/logout",
+    // },
+  ],
+  links: {
+    github: "https://github.com/brunoreiis",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui-docs-v2.vercel.app",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { Link, Snippet, Code, button as buttonStyles } from "@nextui-org/react";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section>
+        
+      </section>
+    </DefaultLayout>
+  );
+}
